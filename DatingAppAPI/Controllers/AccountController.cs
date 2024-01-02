ab9b48b3856afab2799cc55542c1242fed49affc
viktorp999 <viktorpetrovski99@gmail.com>
2024-01-02 17:53:58 +0100

    Added Base Api Authentication

---
+++ DatingAppAPI/Controllers/AccountController.cs
@@ -0,0 +1,74 @@
+﻿using DatingAppAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+using DatingAppAPI.Entities;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using DatingAppAPI.DTOs;
+
+namespace DatingAppAPI.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        private async Task<bool> IsUserExist(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+
+            if (await IsUserExist(registerDto.UserName))
+            {
+                return BadRequest("User Name is taken!");
+            }
+
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerDto.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid username!");
+            }
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                {
+                    return Unauthorized("Invalid Password!");
+                }
+            }
+
+            return user;
+        }
+    }
+}
---
+++ DatingAppAPI/DTOs/RegisterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DatingAppAPI.DTOs
+{
+    public class RegisterDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
